2938668808a97c84d0ee1be179c1812cd8037aa7
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-27 13:12:08 -0300

    agregando before/after a titulos

--- src/components/MainCards.tsx
+++ src/components/MainCards.tsx
@@ -57,8 +57,8 @@ const MainCards = () => {
   return (
     <section className="main-cards w-11/12 mx-auto" id="main-cards">
       <div className="flex flex-col gap-4 justify-center items-center text-center py-8">
-        <h2 className="uppercase font-bold text-3xl">
-          The Book guide <span className="text-cyan-400">online</span> book
+        <h2 className="uppercase font-bold text-3xl section__title">
+          The Book guide <span className="text__accent-color">online</span> book
           store
         </h2>
         <p>
