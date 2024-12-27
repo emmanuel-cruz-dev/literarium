5ae3b60eb493e671bb66182c2c2fe7efc73341db
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-27 15:12:04 -0300

    maincards responsive

--- src/components/MainCards.tsx
+++ src/components/MainCards.tsx
@@ -74,7 +74,7 @@ const MainCards = () => {
         </p>
       </div>
       <article>
-        <div className="grid grid-cols-4 gap-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
           <CardArticle />
         </div>
       </article>
