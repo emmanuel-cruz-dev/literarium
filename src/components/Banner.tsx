20fef91c973f937f066654a93f025a431894f1bf
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-27 15:09:28 -0300

    banner responsive

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,11 @@
 const Banner = () => {
   return (
     <article
-      className="banner flex flex-col lg:flex-row items-center justify-between text-center py-12 px-16 bg-gray-800 text-white"
+      className="banner flex flex-col gap-4 lg:flex-row items-center justify-between text-center py-12 px-3 lg:px-16 bg-gray-800 text-white"
       id="banner"
     >
       <div className="flex flex-col gap-1">
-        <h2 className="uppercase font-bold text-[1.65rem]">
+        <h2 className="uppercase font-bold text-xl lg:text-[1.65rem]">
           The World's Largest Online Bookstore
         </h2>
         <p className="text-[17px]">
