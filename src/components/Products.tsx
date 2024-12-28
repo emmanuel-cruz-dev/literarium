06982a0b92526c762c85f3ea2db96f155da21540
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-28 18:21:29 -0300

    grid-cols responsive en Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -4,7 +4,7 @@ const Products = () => {
   return (
     <section className="products w-full" id="products">
       <article className="w-11/12 mx-auto py-20">
-        <div className="w-full grid grid-cols-3 justify-between">
+        <div className="w-full grid grid-cols-1 gap-12 md:grid-cols-2 lg:gap-0 lg:grid-cols-3 justify-between">
           <CarouselItems title="New Product" />
           <CarouselItems title="Best Seller" />
           <CarouselItems title="Pre-Order" />
