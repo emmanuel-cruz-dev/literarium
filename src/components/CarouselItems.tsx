f0e1ce5af0b9cd91b4ac2e158f51ccf8edbb2d47
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-28 15:16:53 -0300

    espaciado y posicion de CarouselItems

--- src/components/CarouselItems.tsx
+++ src/components/CarouselItems.tsx
@@ -2,26 +2,27 @@ import { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import type { Swiper as SwiperType } from "swiper";
 import { Grid, Autoplay } from "swiper/modules";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 const CarouselItems = () => {
   const swiperRef = useRef<SwiperType>();
 
   return (
-    <article>
-      <div>
-        <h2 className="uppercase">New Product</h2>
+    <article className="w-80 flex flex-col gap-4">
+      <div className="flex justify-between border-b border-slate-300 pb-4">
+        <h2 className="uppercase text-2xl">New Product</h2>
         <div className="flex gap-2">
           <button
             onClick={() => swiperRef.current?.slidePrev()}
-            className="p-2 bg-gray-200 hover:bg-gray-300 rounded-full"
+            className="p-2 bg-gray-200/60 text-gray-600 hover:bg-gray-300"
           >
-            ← {/* Puedes usar un ícono aquí */}
+            <AiOutlineLeft />
           </button>
           <button
             onClick={() => swiperRef.current?.slideNext()}
-            className="p-2 bg-gray-200 hover:bg-gray-300 rounded-full"
+            className="p-2 bg-gray-200/60 text-gray-600 hover:bg-gray-300"
           >
-            → {/* Puedes usar un ícono aquí */}
+            <AiOutlineRight />
           </button>
         </div>
       </div>
