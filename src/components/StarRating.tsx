ae193de27578a904802aa4aaf4fb4672003f1697
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-28 18:01:43 -0300

    funcionalidad de estrellas, seleccion persistente

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -3,6 +3,7 @@ import { MdStarOutline, MdStar } from "react-icons/md";
 
 const StarRating = () => {
   const [hoveredStar, setHoveredStar] = useState<number | null>(null); // Estado para la estrella en hover
+  const [selectedRating, setSelectedRating] = useState(0); // Estado para la calificación seleccionada
 
   const stars = [1, 2, 3, 4, 5]; // Representa el número de estrellas
 
@@ -13,9 +14,11 @@ const StarRating = () => {
           key={star}
           onMouseEnter={() => setHoveredStar(star)} // Cambiar la estrella en hover
           onMouseLeave={() => setHoveredStar(null)} // Resetear el hover al salir
+          onClick={() => setSelectedRating(star)} // Guardar la calificación seleccionada
           style={{ cursor: "pointer", fontSize: "20px" }}
         >
-          {hoveredStar !== null && hoveredStar >= star ? (
+          {(hoveredStar !== null && hoveredStar >= star) ||
+          selectedRating >= star ? (
             <MdStar className="text-yellow-400" />
           ) : (
             <MdStarOutline className="text-yellow-400" />
