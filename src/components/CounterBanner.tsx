750fc63031749d577e04140cc6a11a448d8bb636
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-29 08:50:19 -0300

    estilos de CounterBanner

--- src/components/CounterBanner.tsx
+++ src/components/CounterBanner.tsx
@@ -8,15 +8,15 @@ interface CounterItemProps {
 const CounterItem: FC<CounterItemProps> = ({ number, title }) => {
   return (
     <div className="text-center">
-      <p className="text-6xl">{number}</p>
-      <p className="text-lg font-bold uppercase">{title}</p>
+      <p className="text-7xl">{number}</p>
+      <p className="text-lg uppercase">{title}</p>
     </div>
   );
 };
 
 const CounterBanner: FC = () => {
   return (
-    <article className="grid grid-cols-4 font-semibold text-white background-accent__item py-20">
+    <article className="grid gap-8 lg:gap-0 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 font-bold text-white background-accent__item py-20">
       <CounterItem number={3578} title="Books to Read" />
       <CounterItem number={589} title="Online Users" />
       <CounterItem number={1254} title="Best Authors" />
