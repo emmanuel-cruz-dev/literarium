a1a8ed7218b94e7675daacd4207f11d27c8594f3
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-30 15:58:57 -0300

    nuevo json preOrder y componente renderizado preparando estructura

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,17 +1,26 @@
 import CarouselItems from "./CarouselItems";
 import newProducts from "../data/newProducts.json";
 import bestSellers from "../data/bestSellers.json";
+import preOrder from "../data/preOrder.json";
 
 const Products = () => {
   return (
     <section className="products w-full" id="products">
       <article className="w-11/12 mx-auto py-20">
         <div className="w-full grid grid-cols-1 gap-12 md:grid-cols-2 lg:gap-0 lg:grid-cols-3 justify-between">
-          <CarouselItems key={1} articleTitle="New Product" arr={newProducts} />
-          <CarouselItems key={2} articleTitle="Best Seller" arr={bestSellers} />
+          <CarouselItems
+            key={1}
+            articleTitle="New Products"
+            arr={newProducts}
+          />
 
-          {/* <CarouselItems articleTitle="Best Seller" />
-          <CarouselItems articleTitle="Pre-Order" /> */}
+          <CarouselItems
+            key={2}
+            articleTitle="Best Sellers"
+            arr={bestSellers}
+          />
+
+          <CarouselItems key={3} articleTitle="Pre-Order" arr={preOrder} />
         </div>
       </article>
     </section>
