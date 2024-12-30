16fd9eea508afecce7684d57c1312af7110e7302
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-30 12:53:01 -0300

    BookGuide renderizado y agregado de props interface exitoso

--- src/components/BookGuide.tsx
+++ src/components/BookGuide.tsx
@@ -1,8 +1,17 @@
+import { FC } from "react";
 import AprendaMafia from "../assets/images/aprenda-de-la-mafia.jpg";
 import ComunidadAnillo from "../assets/images/comunidad-anillo.jpg";
 import HarryPotter from "../assets/images/harry-potter.jpg";
 import It from "../assets/images/it.jpg";
 import StarsElement from "./StarsElement";
+
+interface BookGuideProps {
+  title: string;
+  author: string;
+  img: string;
+  rating: number;
+  price: number;
+}
 
 const books = [
   {
@@ -39,12 +48,18 @@ const books = [
   },
 ];
 
-const BookGuideItem = () => {
+const BookGuideItem: FC<BookGuideProps> = ({
+  title,
+  author,
+  img,
+  rating,
+  price,
+}) => {
   return (
     <article className="products__article-item flex flex-col border border-slate-200 hover:border-transparent">
       <div className="relative mt-6 w-9/12 mx-auto">
         <div className="relative">
-          <img className="border-2 border-white" src={AprendaMafia} alt="" />
+          <img className="border-2 border-white" src={img} alt="" />
           <span className="absolute top-4 left-0 bottom-4 -right-2 z-[-1] bg-slate-300"></span>
         </div>
         <a href="#" className="book-guide__plus-item">
@@ -53,14 +68,14 @@ const BookGuideItem = () => {
       </div>
       <div className="flex flex-col gap-2 text-center border-b border-slate-200 py-4 px-6">
         <h2 className="products__card-title leading-tight font-bold text-xl text-slate-700">
-          Basic Time Management Course
+          {title}
         </h2>
-        <p className="text-slate-500">Sed ut perspiciatis</p>
+        <p className="text-slate-500">{author}</p>
       </div>
       <div className="flex justify-between items-center relative py-3 px-3">
-        <span className="font-semibold text-lg text-slate-500">$24.75</span>
+        <span className="font-semibold text-lg text-slate-500">${price}</span>
         <div>
-          <StarsElement num={4} />
+          <StarsElement num={rating} />
         </div>
         <a
           className="book-guide__btn absolute top-0 left-[-.9px] bottom-[-.9px] right-[-.9px] flex items-center text-lg justify-center font-bold text-white text-center background-accent__item uppercase"
@@ -89,10 +104,9 @@ const BookGuide = () => {
         </div>
         <article className="py-6">
           <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-            <BookGuideItem />
-            <BookGuideItem />
-            <BookGuideItem />
-            <BookGuideItem />
+            {books.map((book) => (
+              <BookGuideItem key={book.id} {...book} />
+            ))}
           </div>
         </article>
       </article>
