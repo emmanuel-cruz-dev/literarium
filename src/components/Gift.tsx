aa7483fed359ec275820a049ddc75740dcdc9883
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-31 12:33:03 -0300

    armado, estilos y mapeado de cards en Gift component

--- src/components/Gift.tsx
+++ src/components/Gift.tsx
@@ -1,13 +1,51 @@
+import { FaGift, FaGraduationCap, FaSmile } from "react-icons/fa";
+import { FC } from "react";
+
+interface GiftCardProps {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
+
+const cards = [
+  {
+    id: 1,
+    icon: <FaGift />,
+    title: "Any Occasion",
+    description:
+      "Perfect for birthdays, holidays, or just because - give the gift of reading to someone special.",
+  },
+];
+
+const GiftCard: FC<GiftCardProps> = ({ icon, title, description }) => {
+  return (
+    <article className="flex flex-col gap-2">
+      <div className="flex gap-2 text-2xl uppercase font-bold">
+        <span>{icon}</span>
+        <h2>{title}</h2>
+      </div>
+      <p>{description}</p>
+    </article>
+  );
+};
+
 const Gift = () => {
   return (
     <section className="blog background-accent__item" id="blog">
       <article className="w-11/12 mx-auto py-12">
         <div className="flex flex-col gap-4 justify-center items-center text-center py-4">
           <h2 className="uppercase font-bold text-3xl text-white section__title gift__title">
-            Book Store Gift Card
+            Bookstore Gift Cards
           </h2>
         </div>
-        <article className="py-6"></article>
+        <article className="py-6 text-white">
+          {cards.map((card) => (
+            <GiftCard key={card.id} {...card} />
+          ))}
+
+          <FaGraduationCap size={24} />
+          <FaSmile size={24} />
+        </article>
       </article>
     </section>
   );
