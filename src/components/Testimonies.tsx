9e00cb6fb4c8cf71cadfa3359bce31752c476c89
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-31 10:44:41 -0300

    estructurando Testimonies carousel card

--- src/components/Testimonies.tsx
+++ src/components/Testimonies.tsx
@@ -1,6 +1,10 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Autoplay, Pagination } from "swiper/modules";
 
+import Img1 from "../assets/images/testimonials-img1.jpg";
+import Img2 from "../assets/images/testimonials-img2.jpg";
+import Img3 from "../assets/images/testimonials-img3.jpg";
+
 const Testimonies = () => {
   return (
     <section className="testimonies" id="testimonies">
@@ -10,21 +14,34 @@ const Testimonies = () => {
         </h2>
         <div>
           <Swiper
-            modules={[Navigation, Autoplay, Pagination]}
+            modules={[Navigation, Pagination]}
             spaceBetween={30}
             slidesPerView={1}
             navigation
             pagination={{ clickable: true }}
             loop={true}
-            autoplay={{
-              delay: 5000,
-              disableOnInteraction: false,
-            }}
-            className="w-96 h-[400px]" // Clases de Tailwind
+            // autoplay={{
+            //   delay: 5000,
+            //   disableOnInteraction: false,
+            // }}
+            className="w-2/3 h-[420px]" // Clases de Tailwind
           >
             <SwiperSlide>
-              <div className="flex justify-center items-center bg-gray-300 h-full">
-                Slide 1
+              <div className="flex flex-col gap-2 justify-center items-center bg-gray-300 h-full">
+                <figure className="rounded-full w-40 overflow-hidden border-4 border-cyan-400">
+                  <img src={Img1} alt="" />
+                </figure>
+                <div className="flex flex-col gap-2 [&>h4]:uppercase justify-center text-center">
+                  <h3 className="text__accent-color uppercase font-bold text-3xl">
+                    Sarah Jefferson
+                  </h3>
+                  <h4>Library Manager</h4>
+                  <p className="text-lg">
+                    "I found exactly what I needed at very affordable prices,
+                    and the books were delivered promptly. Excellent
+                    service—highly recommended!"
+                  </p>
+                </div>
               </div>
             </SwiperSlide>
             <SwiperSlide>
