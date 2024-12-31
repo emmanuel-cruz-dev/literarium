5a5ff73b01309105a701b36d4b46e91280739a19
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-31 11:00:27 -0300

    preparando testimoniesArr para mapeo de tarjetas en Testimonies

--- src/components/Testimonies.tsx
+++ src/components/Testimonies.tsx
@@ -5,11 +5,58 @@ import Img1 from "../assets/images/testimonials-img1.jpg";
 import Img2 from "../assets/images/testimonials-img2.jpg";
 import Img3 from "../assets/images/testimonials-img3.jpg";
 
+const testimoniesArr = [
+  {
+    id: 1,
+    image: Img1,
+    name: "Sarah Jefferson",
+    ocupation: "Library Manager",
+    description:
+      "I found exactly what I needed at very affordable prices, and the books were delivered promptly. Excellent service—highly recommended!",
+  },
+  {
+    id: 2,
+    image: Img2,
+    name: "Julia Burns",
+    ocupation: "The Writer",
+    description:
+      "Two or three times a year, especially after Christmas, my family buys from the Book Library—it has become a tradition!",
+  },
+  {
+    id: 3,
+    image: Img3,
+    name: "Anna Hawke",
+    ocupation: "Marketing Manager",
+    description:
+      "Thank you for your support over the years! I would gladly recommend you to my friends.",
+  },
+];
+
+const TestimoniesCard = () => {
+  return (
+    <article className="flex flex-col gap-5 justify-center items-center h-full">
+      <figure className="rounded-full w-40 overflow-hidden border-4 border-cyan-400">
+        <img src={Img1} alt="" />
+      </figure>
+      <div className="flex flex-col gap-3 [&>h4]:uppercase justify-center text-center text-white">
+        <h3 className="text__accent-color uppercase font-bold text-3xl">
+          Sarah Jefferson
+        </h3>
+        <h4>Library Manager</h4>
+        <p className="text-lg">
+          "I found exactly what I needed at very affordable prices, and the
+          books were delivered promptly. Excellent service—highly recommended!"
+        </p>
+      </div>
+    </article>
+  );
+};
+
 const Testimonies = () => {
   return (
     <section className="testimonies" id="testimonies">
-      <article>
-        <h2 className="uppercase text-white font-semibold text-3xl">
+      <article className="py-12">
+        <h2 className="text-center uppercase text-white font-semibold text-3xl">
           What Our Clients Are Saying About Us
         </h2>
         <div>
@@ -27,22 +74,7 @@ const Testimonies = () => {
             className="w-2/3 h-[420px]" // Clases de Tailwind
           >
             <SwiperSlide>
-              <div className="flex flex-col gap-2 justify-center items-center bg-gray-300 h-full">
-                <figure className="rounded-full w-40 overflow-hidden border-4 border-cyan-400">
-                  <img src={Img1} alt="" />
-                </figure>
-                <div className="flex flex-col gap-2 [&>h4]:uppercase justify-center text-center">
-                  <h3 className="text__accent-color uppercase font-bold text-3xl">
-                    Sarah Jefferson
-                  </h3>
-                  <h4>Library Manager</h4>
-                  <p className="text-lg">
-                    "I found exactly what I needed at very affordable prices,
-                    and the books were delivered promptly. Excellent
-                    service—highly recommended!"
-                  </p>
-                </div>
-              </div>
+              <TestimoniesCard />
             </SwiperSlide>
             <SwiperSlide>
               <div className="flex justify-center items-center bg-gray-400 h-full">
