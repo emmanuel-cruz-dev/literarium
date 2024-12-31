db7d065db43a62a30d1891f428e3025ca8e650bb
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-31 15:35:56 -0300

    estilos en PricePackage price card

--- src/components/PricePackage.tsx
+++ src/components/PricePackage.tsx
@@ -1,10 +1,10 @@
 const PriceCard = () => {
   return (
-    <article className="text-center">
-      <h2>Personal</h2>
-      <div>
-        <p>$77</p>
-        <p>Per Month</p>
+    <article className="price-card-container flex flex-col justify-center items-center gap-6 text-center py-6 bg-slate-100/70 hover:bg-white transition-colors duration-300 ease-in-out">
+      <h2 className="uppercase">Personal</h2>
+      <div className="price-card__price-circle">
+        <p className="text-3xl font-light">$77</p>
+        <p className="text-sm">Per Month</p>
       </div>
       <ul>
         <li>250 E-Books</li>
@@ -12,13 +12,16 @@ const PriceCard = () => {
         <li>5 Books</li>
         <li>36 Magazines</li>
       </ul>
+      <a className="btn" href="#">
+        Purchase Now
+      </a>
     </article>
   );
 };
 
 const PricePackage = () => {
   return (
-    <section className="price-package h-screen" id="blog">
+    <section className="price-package" id="blog">
       <article className="w-11/12 mx-auto py-12">
         <div className="flex flex-col gap-4 justify-center items-center text-center py-4">
           <h2 className="uppercase font-bold text-3xl section__title">
