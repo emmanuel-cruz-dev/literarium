3125a9f30fca083f12a0f94a2e234e06870634b5
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2025-01-01 15:11:59 -0300

    estructura y estilos seccion Newsletter

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,7 +1,7 @@
 const Newsletter = () => {
   return (
     <section className="newsletter background-accent__item" id="newsletter">
-      <article className="flex flex-col gap-6 w-11/12 mx-auto py-12 px-4">
+      <article className="flex flex-col gap-6 w-11/12 mx-auto py-12">
         <div className="flex flex-col gap-4 justify-center items-center text-center py-4">
           <h2 className="uppercase font-bold text-3xl text-white section__title gift__title">
             Subscribe to our Newsletter for the Latest News
@@ -12,12 +12,12 @@ const Newsletter = () => {
             to your email every week.
           </p>
         </div>
-        <article className="flex w-2/3 items-center mx-auto py-12 lg:py-4 bg-white text-white">
-          <form className="flex w-full px-4">
+        <article className="flex w-full lg:w-2/3 p-3 items-center mx-auto bg-white text-white">
+          <form className="flex w-full">
             <input
               type="email"
               placeholder="Your email address"
-              className="w-5/6 p-4 py-2 rounded-xs"
+              className="w-5/6 lg:p-4 py-2 rounded-xs"
             />
             <button className="background-accent__item text-lg uppercase font-bold py-2 px-6 rounded-xs hover:bg-black transition-colors duration-300 ease-in-out">
               Subscribe
