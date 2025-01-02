ff3d1edc8927e51c042c6f2f1a03ee1e942f373a
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2025-01-02 16:38:50 -0300

    estructurando Welcome

--- src/components/routes/about/AboutWelcome.tsx
+++ src/components/routes/about/AboutWelcome.tsx
@@ -1,3 +1,5 @@
+import PeopleImage from "../../../assets/images/about-people-img.jpg";
+
 const AboutWelcome = () => {
   return (
     <section className="main-cards w-11/12 mx-auto py-12" id="main-cards">
@@ -6,11 +8,40 @@ const AboutWelcome = () => {
           Welcome to <span className="text__accent-color">Literarium</span>
         </h2>
       </div>
-      <article>
-        <figure>
-          <img src="" alt="" />
+      <article className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-12 lg:gap-8 py-6">
+        <figure className="col-span-1">
+          <img src={PeopleImage} alt="People" />
         </figure>
-        <div></div>
+        <div className="col-span-2 tracking-wider flex flex-col gap-4 items-center">
+          <p>
+            PLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat.
+          </p>
+          <p>
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
+          </p>
+        </div>
       </article>
     </section>
   );
