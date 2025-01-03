294ce8811434d4fee2d6535d897487dbea52c522
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2025-01-03 15:48:26 -0300

    estructurando Mission

--- src/components/routes/about/AboutMission.tsx
+++ src/components/routes/about/AboutMission.tsx
@@ -1,5 +1,71 @@
 const AboutMission = () => {
-  return <div>AboutMission</div>;
+  return (
+    <section className="bg-gray-800 text-white" id="about-mission">
+      <article className="w-11/12 mx-auto py-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mx-auto">
+          <div>Info</div>
+          <div>
+            <details>
+              <summary>Our Mission and Vision</summary>
+              <div>
+                <p>
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life
+                  accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                  non cupidatat skateboard dolor brunch. Food truck quinoa
+                  nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
+                  aliqua put a bird on it squid single-origin coffee nulla
+                  assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft
+                  beer labore wes anderson cred nesciunt sapiente ea proident.
+                  Ad vegan excepteur butcher vice lomo. Leggings occaecat craft
+                  beer farm-to-table, raw denim aesthetic synth nesciunt you
+                  probably haven't heard of them accusamus labore sustainable
+                  VHS.
+                </p>
+              </div>
+            </details>
+
+            <details>
+              <summary>24 Hours full video support</summary>
+              <div>
+                <p>
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life
+                  accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                  non cupidatat skateboard dolor brunch. Food truck quinoa
+                  nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
+                  aliqua put a bird on it squid single-origin coffee nulla
+                  assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft
+                  beer labore wes anderson cred nesciunt sapiente ea proident.
+                  Ad vegan excepteur butcher vice lomo. Leggings occaecat craft
+                  beer farm-to-table, raw denim aesthetic synth nesciunt you
+                  probably haven't heard of them accusamus labore sustainable
+                  VHS.
+                </p>
+              </div>
+            </details>
+
+            <details>
+              <summary>User-friendly Design</summary>
+              <div>
+                <p>
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life
+                  accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                  non cupidatat skateboard dolor brunch. Food truck quinoa
+                  nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
+                  aliqua put a bird on it squid single-origin coffee nulla
+                  assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft
+                  beer labore wes anderson cred nesciunt sapiente ea proident.
+                  Ad vegan excepteur butcher vice lomo. Leggings occaecat craft
+                  beer farm-to-table, raw denim aesthetic synth nesciunt you
+                  probably haven't heard of them accusamus labore sustainable
+                  VHS.
+                </p>
+              </div>
+            </details>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default AboutMission;
