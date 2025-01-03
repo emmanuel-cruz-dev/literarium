78a7ae89c208006259410bd799e3dcac32b8c9bb
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2025-01-03 16:42:20 -0300

    agregando array de objetos para mapear

--- src/components/routes/about/AboutMission.tsx
+++ src/components/routes/about/AboutMission.tsx
@@ -1,10 +1,40 @@
 import MissionImg1 from "../../../assets/images/mission-img1.jpg";
+import MissionImg2 from "../../../assets/images/mission-img2.jpg";
+import MissionImg3 from "../../../assets/images/mission-img3.jpg";
+import MissionImg4 from "../../../assets/images/mission-img4.jpg";
+
+const items = [
+  {
+    id: 1,
+    title: "Info",
+    img: MissionImg1,
+    text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+  },
+  {
+    id: 2,
+    title: "Our Mission and Vision",
+    img: MissionImg2,
+    text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+  },
+  {
+    id: 3,
+    title: "24 Hours full video support",
+    img: MissionImg3,
+    text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+  },
+  {
+    id: 4,
+    title: "User-friendly Design",
+    img: MissionImg4,
+    text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+  },
+];
 
 const AboutMission = () => {
   return (
     <section className="about-mission" id="about-mission">
       <article className="w-11/12 mx-auto py-12">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mx-auto">
+        <div className="leading-relaxed grid grid-cols-1 md:grid-cols-2 gap-8 mx-auto">
           <div>
             <div className="relative">
               <img src={MissionImg1} alt="mission" />
@@ -29,9 +59,9 @@ const AboutMission = () => {
               <summary>Our Mission and Vision</summary>
               <div>
                 <p>
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life
-                  accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                  non cupidatat skateboard dolor brunch. Food truck quinoa
+                  Our Mission and Vision cliche reprehenderit, enim eiusmod high
+                  life accusamus terry richardson ad squid. 3 wolf moon officia
+                  aute, non cupidatat skateboard dolor brunch. Food truck quinoa
                   nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
                   aliqua put a bird on it squid single-origin coffee nulla
                   assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft
@@ -48,17 +78,17 @@ const AboutMission = () => {
               <summary>24 Hours full video support</summary>
               <div>
                 <p>
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life
-                  accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                  non cupidatat skateboard dolor brunch. Food truck quinoa
-                  nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
-                  aliqua put a bird on it squid single-origin coffee nulla
-                  assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft
-                  beer labore wes anderson cred nesciunt sapiente ea proident.
-                  Ad vegan excepteur butcher vice lomo. Leggings occaecat craft
-                  beer farm-to-table, raw denim aesthetic synth nesciunt you
-                  probably haven't heard of them accusamus labore sustainable
-                  VHS.
+                  24 Hours full video pariatur cliche reprehenderit, enim
+                  eiusmod high life accusamus terry richardson ad squid. 3 wolf
+                  moon officia aute, non cupidatat skateboard dolor brunch. Food
+                  truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
+                  tempor, sunt aliqua put a bird on it squid single-origin
+                  coffee nulla assumenda shoreditch et. Nihil anim keffiyeh
+                  helvetica, craft beer labore wes anderson cred nesciunt
+                  sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                  Leggings occaecat craft beer farm-to-table, raw denim
+                  aesthetic synth nesciunt you probably haven't heard of them
+                  accusamus labore sustainable VHS.
                 </p>
               </div>
             </details>
@@ -67,17 +97,17 @@ const AboutMission = () => {
               <summary>User-friendly Design</summary>
               <div>
                 <p>
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life
-                  accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                  non cupidatat skateboard dolor brunch. Food truck quinoa
-                  nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
-                  aliqua put a bird on it squid single-origin coffee nulla
-                  assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft
-                  beer labore wes anderson cred nesciunt sapiente ea proident.
-                  Ad vegan excepteur butcher vice lomo. Leggings occaecat craft
-                  beer farm-to-table, raw denim aesthetic synth nesciunt you
-                  probably haven't heard of them accusamus labore sustainable
-                  VHS.
+                  User friendly design pariatur cliche reprehenderit, enim
+                  eiusmod high life accusamus terry richardson ad squid. 3 wolf
+                  moon officia aute, non cupidatat skateboard dolor brunch. Food
+                  truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
+                  tempor, sunt aliqua put a bird on it squid single-origin
+                  coffee nulla assumenda shoreditch et. Nihil anim keffiyeh
+                  helvetica, craft beer labore wes anderson cred nesciunt
+                  sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                  Leggings occaecat craft beer farm-to-table, raw denim
+                  aesthetic synth nesciunt you probably haven't heard of them
+                  accusamus labore sustainable VHS.
                 </p>
               </div>
             </details>
