028061bbf341049ac831347d15e73227547275fa
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2025-01-03 17:02:44 -0300

    styles colores de texto en componente y clases pages

--- src/components/routes/about/AboutMission.tsx
+++ src/components/routes/about/AboutMission.tsx
@@ -76,7 +76,7 @@ const AboutMission = () => {
   return (
     <section className="about-mission" id="about-mission">
       <article className="w-11/12 mx-auto py-12">
-        <div className="leading-relaxed grid grid-cols-1 md:grid-cols-2 gap-8 mx-auto">
+        <div className="leading-relaxed text-gray-600 grid grid-cols-1 md:grid-cols-2 gap-8 mx-auto">
           <MissionRender arr={items} />
           <div>
             <details>
