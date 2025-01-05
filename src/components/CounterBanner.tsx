00aae00cdca938122cf533752f155ece881016fe
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2025-01-05 13:55:15 -0300

    agregando plus a currentNumber CounterBanner

--- src/components/CounterBanner.tsx
+++ src/components/CounterBanner.tsx
@@ -32,7 +32,9 @@ const CounterItem: FC<CounterItemProps> = ({ number, title }) => {
 
   return (
     <div className="text-center">
-      <p className="text-7xl">{currentNumber}</p>
+      <p className="text-7xl">
+        {currentNumber > 400 ? currentNumber + "+" : currentNumber}
+      </p>
       <p className="w-2/3 mx-auto uppercase">{title}</p>
     </div>
   );
