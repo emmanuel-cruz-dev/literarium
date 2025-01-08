165f0d65ae52d05e43b07d0f7973dba73a452b77
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2025-01-08 11:45:48 -0300

    newarrivals component creado, estructura y estilos

--- src/components/routes/products/ProductsContent.tsx
+++ src/components/routes/products/ProductsContent.tsx
@@ -2,7 +2,6 @@ import { FaHeart, FaShoppingCart, FaLink } from "react-icons/fa";
 import BookImg1 from "../../../assets/images/books-list1.jpg";
 import { SearchItem, AsideItemList } from "../blog/BlogContent";
 import CarouselItems from "../../CarouselItems";
-// import preOrder from "../data/preOrder.json";
 import preOrder from "../../../data/preOrder.json";
 
 const data1 = [
@@ -14,6 +13,27 @@ const data1 = [
   "History",
   "Self-Help",
   "Children & Young Adult",
+];
+
+const arrivalsData = [
+  {
+    id: 1,
+    img: BookImg1,
+    title: "consetetur sadip scing",
+    text: "Sed diam nonumy eirmod tempor invidunt ut labore et dolore.",
+  },
+  {
+    id: 2,
+    img: BookImg1,
+    title: "Consetetur sadi elitr",
+    text: "Magna aliquyam erat, sed diam voluptua. At vero eos et accusam.",
+  },
+  {
+    id: 3,
+    img: BookImg1,
+    title: "Conset sading elitr",
+    text: "At vero eos et accusam et justo duo dolores et ea rebum.",
+  },
 ];
 
 const ProductCard = () => {
@@ -59,14 +79,42 @@ const ProductCard = () => {
   );
 };
 
+const NewArrivals = () => {
+  return (
+    <article className="new-arrivals__container flex flex-col gap-4">
+      <h2 className="blog-content__aside__item-title">New Arrivals</h2>
+      {arrivalsData.map((item) => (
+        <article className="new-arrivals__card flex gap-4 border border-slate-300 p-4 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+          <a
+            href="#"
+            className="new-arrivals__img-container w-20 flex-shrink-0"
+          >
+            <img
+              className="new-arrivals__img w-full h-full object-cover"
+              src={item.img}
+              alt=""
+            />
+          </a>
+          <div className="flex flex-col gap-2">
+            <h2 className="uppercase font-bold text-black leading-tight">
+              {item.title}
+            </h2>
+            <p className="text-sm">{item.text}</p>
+          </div>
+        </article>
+      ))}
+    </article>
+  );
+};
+
 const ProductsContent = () => {
   return (
     <section className="products-content bg-neutral-100" id="products-content">
       <article className="w-11/12 mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 py-6 text-neutral-600 font-light">
         <aside className="md:col-span-1 flex flex-col gap-6">
           <SearchItem />
+          <NewArrivals />
           <AsideItemList title="Categories" dataArr={data1} />
-
           <CarouselItems key={3} articleTitle="Pre-Order" arr={preOrder} />
         </aside>
         <article className="auto-rows-[440px] md:col-span-3 grid md:grid-cols-2 lg:grid-cols-3 gap-8">
