5cea8d75fa16595fa6d83bba883b05c2a7941c66
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2025-01-08 08:40:06 -0300

    orden en ProductsContent

--- src/components/routes/products/ProductsContent.tsx
+++ src/components/routes/products/ProductsContent.tsx
@@ -3,10 +3,7 @@ import BookImg1 from "../../../assets/images/books-list1.jpg";
 
 const ProductCard = () => {
   return (
-    <a
-      href="#"
-      className="products-content__article-container flex flex-col gap-4 mb-12 bg-white"
-    >
+    <article className="products-content__article-container flex flex-col gap-4 mb-12 bg-white">
       <img src={BookImg1} alt="" />
       <div className="flex flex-col gap-2 text-center">
         <div>
@@ -38,16 +35,16 @@ const ProductCard = () => {
           </div>
         </div>
       </div>
-    </a>
+    </article>
   );
 };
 
 const ProductsContent = () => {
   return (
     <section className="products-content bg-neutral-100" id="products-content">
-      <article className="w-11/12 mx-auto grid grid-cols-4 gap-8 py-6 text-neutral-600 font-light">
-        <aside className="col-span-1 bg-cyan-400"></aside>
-        <article className="col-span-3 grid grid-cols-3 gap-8">
+      <article className="w-11/12 mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 py-6 text-neutral-600 font-light">
+        <aside className="md:col-span-1 bg-cyan-400"></aside>
+        <article className="md:col-span-3 grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           <ProductCard />
           <ProductCard />
           <ProductCard />
