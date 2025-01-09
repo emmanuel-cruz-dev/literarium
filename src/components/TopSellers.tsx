59938145fa3fd48fd2bd280b8c166ca20ff69bfa
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2025-01-09 15:23:54 -0300

    editando estilos en TopSellers, imagenes con padding y titulos mas alto

--- src/components/TopSellers.tsx
+++ src/components/TopSellers.tsx
@@ -65,9 +65,9 @@ const BookCard: FC<BookCardProps> = ({
     <article key={id} className="top-sellers__book-card bg-white">
       <div className="flex flex-col items-center">
         <figure className="book-card__image">
-          <img src={img} alt="Book Image" />
+          <img className="p-6" src={img} alt="Book Image" />
         </figure>
-        <h3 className="py-2 text-lg">{title}</h3>
+        <h3 className="-mt-4 pb-3 text-lg">{title}</h3>
       </div>
       <div className="top-sellers__hidden-menu">
         <div className="top-sellers__hidden-menu__body">
@@ -76,7 +76,7 @@ const BookCard: FC<BookCardProps> = ({
           <div className="flex text-2xl">
             <StarsElement num={rating} />
           </div>
-          <span className="text-4xl font-thin">${price}</span>
+          <span className="text-3xl font-thin">${price}</span>
           <button className="font-semibold uppercase border-2 border-white py-2 px-4 hover:bg-white hover:text-gray-900 transition-colors duration-300 ease-in-out">
             Add to Cart
           </button>
