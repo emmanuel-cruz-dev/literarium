6ae93a3846d1f4221601a22682e123fc1424776d
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2025-01-10 12:50:17 -0300

    iconos agregados de react-icons

--- src/components/routes/blog/BlogMain.tsx
+++ src/components/routes/blog/BlogMain.tsx
@@ -1,27 +1,46 @@
+import { FaSearch, FaShareAlt, FaHeart } from "react-icons/fa";
 import MainBlogWriter1 from "../../../assets/images/main-blog-writer1.jpg";
 import MainBlogImg1 from "../../../assets/images/main-blog-img1.jpg";
 
 const BlogMainArticle = () => {
   return (
-    <article>
+    <article className="grid grid-cols-5">
       <div>
-        <figure>
-          <img src={MainBlogWriter1} alt="" />
-        </figure>
         <div>
-          <p>Writen By</p>
-          <h2>Belly</h2>
+          <figure>
+            <img src={MainBlogWriter1} alt="" />
+          </figure>
+          <div>
+            <p>Writen By</p>
+            <h2>Belly</h2>
+          </div>
+        </div>
+        <div>
+          <p>
+            26
+            <span>th</span>
+          </p>
+          <p>August 2024</p>
         </div>
       </div>
-      <div>
+
+      <div className="col-span-4">
+        <div>
+          <img src={MainBlogImg1} alt="" />
+          <div>
+            <FaSearch />
+            <FaShareAlt />
+            <FaHeart />
+          </div>
+        </div>
+        <h2>NerdCon Stories, Day Two</h2>
         <p>
-          26
-          <span>th</span>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam.ipsam voluptatem
+          quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+          magni dolores eos qui ratione voluptatem sequi nesciunt.
         </p>
-        <p>August 2024</p>
-      </div>
-      <div>
-        <img src={MainBlogImg1} alt="" />
+        <button>Read More</button>
       </div>
     </article>
   );
@@ -30,7 +49,6 @@ const BlogMainArticle = () => {
 const BlogMain = () => {
   return (
     <article className="lg:col-span-3 mx-auto flex flex-col gap-4">
-      <h2>blog</h2>
       <BlogMainArticle />
     </article>
   );
