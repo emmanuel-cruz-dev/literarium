6a1dd6e506bb16cfa49c45f65ddc0dd76e184c7c
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2025-01-10 13:17:10 -0300

    ul para navegacion al final de BlogMain

--- src/components/routes/blog/BlogMain.tsx
+++ src/components/routes/blog/BlogMain.tsx
@@ -1,10 +1,15 @@
-import { FaSearch, FaShareAlt, FaHeart } from "react-icons/fa";
+import {
+  FaSearch,
+  FaShareAlt,
+  FaHeart,
+  FaAngleDoubleRight,
+} from "react-icons/fa";
 import MainBlogWriter1 from "../../../assets/images/main-blog-writer1.jpg";
 import MainBlogImg1 from "../../../assets/images/main-blog-img1.jpg";
 
 const BlogMainArticle = () => {
   return (
-    <article className="grid grid-cols-5">
+    <article className="grid grid-cols-5 border-b border-neutral-200 pb-8">
       <div>
         <div>
           <figure>
@@ -25,22 +30,27 @@ const BlogMainArticle = () => {
       </div>
 
       <div className="col-span-4">
-        <div>
+        <div className="relat">
           <img src={MainBlogImg1} alt="" />
-          <div>
+          <div className="flex">
             <FaSearch />
             <FaShareAlt />
             <FaHeart />
           </div>
         </div>
-        <h2>NerdCon Stories, Day Two</h2>
-        <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam.ipsam voluptatem
-          quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
-          magni dolores eos qui ratione voluptatem sequi nesciunt.
-        </p>
-        <button>Read More</button>
+        <div className="flex flex-col gap-4">
+          <h2 className="uppercase font-bold text-2xl text-black">
+            NerdCon Stories, Day Two
+          </h2>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam.ipsam
+            voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+            consequuntur magni dolores eos qui ratione voluptatem sequi
+            nesciunt.
+          </p>
+          <button className="btn">Read More</button>
+        </div>
       </div>
     </article>
   );
@@ -48,8 +58,35 @@ const BlogMainArticle = () => {
 
 const BlogMain = () => {
   return (
-    <article className="lg:col-span-3 mx-auto flex flex-col gap-4">
+    <article className="lg:col-span-3 mx-auto flex flex-col gap-8">
       <BlogMainArticle />
+      <BlogMainArticle />
+      <BlogMainArticle />
+      <BlogMainArticle />
+      <div>
+        <ul className="w-fit flex justify-center items-center mx-auto border border-neutral-400 [&>li]:py-3 [&>li]:px-4 [&>li]:font-medium [&>li]:border-r [&>li]:border-neutral-400">
+          <li>
+            <a href="#">1</a>
+          </li>
+          <li>
+            <a href="#">2</a>
+          </li>
+          <li>
+            <a href="#">3</a>
+          </li>
+          <li>
+            <a href="#">4</a>
+          </li>
+          <li>
+            <a href="#">5</a>
+          </li>
+          <li>
+            <a className="text-sm" href="#">
+              <FaAngleDoubleRight />
+            </a>
+          </li>
+        </ul>
+      </div>
     </article>
   );
 };
