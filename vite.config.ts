ce7373c387c9b96d423749045f2fa3f56e91d569
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2025-01-11 13:45:35 -0300

    robots.txt agregado y configurado

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteStaticCopy({
+    targets: [
+      { src: 'robots.txt', dest: '.' },
+      
+    ],
+  })],
   build: {
     rollupOptions: {
       input: {
