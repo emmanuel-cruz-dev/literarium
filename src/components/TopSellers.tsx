c8e09a9c19cd76676eadb45197c36cfef4032a87
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-20 17:07:33 -0300

    datos de libros en TopSellers actualizados

--- src/components/TopSellers.tsx
+++ src/components/TopSellers.tsx
@@ -1,40 +1,40 @@
 import { FC } from "react";
-import JuegoTronos from "../assets/images/knight-seven-kingdoms.webp";
-import DoceReglas from "../assets/images/rules-life.webp";
-import LibertadPrimera from "../assets/images/first-last-freedom.webp";
-import PoderHabitos from "../assets/images/power-habit.webp";
+import KnightSevenKingdoms from "../assets/images/knight-seven-kingdoms.webp";
+import RulesLife from "../assets/images/rules-life.webp";
+import FirstLastFreedom from "../assets/images/first-last-freedom.webp";
+import PowerHabits from "../assets/images/power-habit.webp";
 import StarsElement from "./StarsElement";
 
 const books = [
   {
     id: 1,
-    title: "Juego de tronos",
+    title: "A Knight of the Seven Kingdoms",
     author: "George R. R. Martin",
-    img: JuegoTronos,
+    img: KnightSevenKingdoms,
     rating: 5,
     price: 692.99,
   },
   {
     id: 2,
-    title: "12 reglas para vivir",
+    title: "12 Rules for Life",
     author: "Jordan Peterson",
-    img: DoceReglas,
+    img: RulesLife,
     rating: 4,
     price: 789.65,
   },
   {
     id: 3,
-    title: "La libertad primera y última",
+    title: "The First and Last Freedom",
     author: "Jiddu Krishnamurti",
-    img: LibertadPrimera,
+    img: FirstLastFreedom,
     rating: 5,
     price: 849.39,
   },
   {
     id: 4,
-    title: "El poder de los hábitos",
+    title: "The Power of Habit",
     author: "Charles Duhigg",
-    img: PoderHabitos,
+    img: PowerHabits,
     rating: 5,
     price: 526.15,
   },
