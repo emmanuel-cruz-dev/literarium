9ba183c5aa8e4758945ea8af24afe055e3887ce3
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-19 16:00:21 -0300

    estilos organizados en carpetas

--- src/pages/ContactUs/Contact/Contact.tsx
+++ src/pages/ContactUs/Contact/Contact.tsx
@@ -1,5 +1,6 @@
 import PagesHero from "../../PagesHero";
 import Content from "../Content/Content";
+import "../../../styles/pages/ContactUs/contact.css";
 
 function Contact() {
   return (
--- src/pages/Products/Products/Products.tsx
+++ src/pages/Products/Products/Products.tsx
@@ -2,6 +2,7 @@ import PagesHero from "../../PagesHero";
 import Content from "../Content/Content";
 import { ProductsContext } from "../../../context/ProductsContext";
 import useToggleVisibility from "../../../hooks/useToggleVisibility";
+import "../../../styles/pages/Products/products.css";
 
 function Products() {
   const { isVisible, handleClick } = useToggleVisibility();
