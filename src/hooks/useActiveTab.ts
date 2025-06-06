3797f5062ddd1becee78835bb7f452d055886104
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-06 16:44:08 -0300

    refactor: remove comments in useActiveTab.ts

--- src/hooks/useActiveTab.ts
+++ src/hooks/useActiveTab.ts
@@ -4,7 +4,7 @@ const useActiveTab = () => {
   const [activeTab, setActiveTab] = useState(0);
 
   const handleClick = (index: number) => {
-    setActiveTab(index - 1); // -1 porque el primer elemento es el index 0
+    setActiveTab(index - 1);
   };
 
   return { activeTab, handleClick };
