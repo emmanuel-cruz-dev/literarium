c76643a4c901415bedb00dc47d6ddc3bf7e0812a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-06 17:09:07 -0300

    feat: adds TagItem component to Tabs folder

---
+++ src/pages/Products/Tabs/TagItem.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import { TagItemProps } from "types/types";
+
+const TagItem: FC<TagItemProps> = ({ arr }) => {
+  return (
+    <div className="flex gap-2 flex-wrap">
+      {arr.map((item, index) => (
+        <a
+          href="#"
+          className="border border-slate-300 py-2 px-3 uppercase font-semibold text-sm hover:bg-neutral-600 hover:text-white transition-colors duration-300 ease-in-out"
+          key={index}
+        >
+          {item}
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default TagItem;
