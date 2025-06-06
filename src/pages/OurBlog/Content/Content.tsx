7c6622c53a79901377bbdbc8389d633f2a2f802e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-06 16:59:26 -0300

    feat: imports StayTunedCard to Content in Blog/OurBlog

--- src/pages/OurBlog/Content/Content.tsx
+++ src/pages/OurBlog/Content/Content.tsx
@@ -3,8 +3,6 @@ import { BlogContext } from "../../../context/BlogContext";
 import {
   FaFacebookF,
   FaTwitter,
-  FaLinkedinIn,
-  FaSkype,
   FaGooglePlusG,
   FaHeart,
   FaChevronLeft,
@@ -28,6 +26,7 @@ import {
 import { instagramImages } from "../../../data/instagramImages";
 import { data1, data2 } from "../../../data/contentListData";
 import { contentPosts } from "../../../data/contentPosts";
+import StayTunedCard from "./StayTunedCard";
 
 function Content() {
   const { isVisible, handleClick } = useContext(BlogContext);
@@ -216,50 +215,7 @@ function Content() {
 
           <AsideItemList title="Categories" dataArr={data1} />
 
-          <div className="flex flex-col gap-4">
-            <h2 className="blog-content__aside__item-title">Stay Tuned</h2>
-            <div className="grid grid-cols-3 items-center border border-slate-300 overflow-hidden">
-              <input
-                name="subscribe"
-                id="subscribe"
-                className="col-span-2 py-2 px-3 focus:outline-none"
-                type="text"
-                placeholder="Subscribe Us"
-              />
-              <button className="w-full h-full text-white background-accent__item uppercase font-bold hover:bg-black transition-colors duration-300 ease-in-out">
-                Submit
-              </button>
-            </div>
-            <div>
-              <ul className="blog-content__aside__unordered-list flex justify-between gap-4 text-lg">
-                <li>
-                  <a href="#">
-                    <FaFacebookF />
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <FaTwitter />
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <FaGooglePlusG />
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <FaSkype />
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <FaLinkedinIn />
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
+          <StayTunedCard />
 
           <div className="flex flex-col gap-4">
             <h2 className="blog-content__aside__item-title">Latest Posts</h2>
