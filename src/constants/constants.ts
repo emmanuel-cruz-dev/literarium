2eea5159f12f2111107fd4653b91fbe8a5b09527
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-06 16:41:00 -0300

    feat: creates and updates constants folder and file

---
+++ src/constants/constants.ts
@@ -0,0 +1,12 @@
+// Loader.tsx
+export const sizeClasses = {
+  sm: "w-6 h-6",
+  md: "w-8 h-8",
+  lg: "w-12 h-12",
+};
+
+export const colorClasses = {
+  primary: "text-blue-600",
+  secondary: "text-gray-600",
+  white: "text-white",
+};
