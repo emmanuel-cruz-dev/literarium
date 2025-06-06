9692e17bc1573c03dc172c8ad69616d79469a4d2
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-06 16:52:30 -0300

    feat: imports SortFilter in Content component

--- src/pages/Products/Content/Content.tsx
+++ src/pages/Products/Content/Content.tsx
@@ -8,6 +8,7 @@ import AsideItemList from "../../OurBlog/Content/AsideItemList";
 import NewArrivals from "./NewArrivals";
 import ProductMain from "./ProductMain";
 import { genresData } from "../../../data/genresData";
+import SortFilter from "./SortFilter";
 
 function Content() {
   const { isVisible } = useContext(ProductsContext);
@@ -17,21 +18,7 @@ function Content() {
       <article className="w-11/12 mx-auto grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-8 py-12 text-neutral-600 font-light">
         <aside className="md:col-span-1 flex flex-col gap-6">
           <SearchItem />
-
-          <article className="new-arrivals__container flex flex-col gap-4">
-            <h2 className="blog-content__aside__item-title">Sort by</h2>
-            <select
-              className="border border-slate-300 py-2 px-3 focus:outline-none bg-inherit text-neutral-600 cursor-pointer"
-              name="sort"
-              id="sort"
-            >
-              <option value="most-relevant">Most Relevant</option>
-              <option value="newest">Newest</option>
-              <option value="lowest-price">Lowest Price</option>
-              <option value="highest-price">Highest Price</option>
-            </select>
-          </article>
-
+          <SortFilter />
           <NewArrivals />
           <AsideItemList title="Categories" dataArr={genresData} />
           <CarouselItems
